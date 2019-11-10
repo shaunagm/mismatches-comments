8c8c9ee9de1ac7e9c17b69f46843f4c736cdd26b
Shauna Gordon-McKeon <shaunagm@gmail.com>
2019-11-09 22:11:35 -0500

    Display comments in datatables.js

---
+++ process_comments.py
@@ -0,0 +1,19 @@
+# load json file
+import json
+
+new_dict = {}
+
+with open('comments.txt') as json_file:
+    data = json.load(json_file)
+    for comment in data:
+        new_dict[comment['id']] = {
+            "body": comment['body'],
+            "url": comment['url'].replace("commitcomment-", "r"),
+            "interview": comment['path']
+        }
+        
+with open('processed_comments.txt', 'w') as outfile:
+    json.dump(new_dict, outfile)
+
+# TODO: process body to generate a list of hashtags?
+
