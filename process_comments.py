d03a2be65bd1ff37e524708902a15f3afee74a40
Shauna Gordon-McKeon <shaunagm@gmail.com>
2019-11-09 23:14:46 -0500

    Fix links

--- process_comments.py
+++ process_comments.py
@@ -1,16 +1,17 @@
-# load json file
 import json
+
 
 new_dict = {"data": []}
 
 with open('comments.txt') as json_file:
     data = json.load(json_file)
-    for comment in data:
-        new_dict["data"].append({
-            "body": comment['body'],
-            "url": comment['url'].replace("commitcomment-", "r"),
-            "interview": comment['path']
-        })
+    for page in data:
+        for comment in page:
+            new_dict["data"].append({
+                "body": comment['body'],
+                "url": comment['html_url'].replace("commitcomment-", "r"),
+                "interview": comment['path']
+            })
 
 with open('processed_comments.txt', 'w') as outfile:
     json.dump(new_dict, outfile)
