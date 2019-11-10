8efe566d9e02b573a4cce0ca20916ca0c781a9fd
Shauna Gordon-McKeon <shaunagm@gmail.com>
2019-11-09 22:27:54 -0500

    Fix data format

--- process_comments.py
+++ process_comments.py
@@ -1,17 +1,17 @@
 # load json file
 import json
 
-new_dict = {}
+new_dict = {"data": []}
 
 with open('comments.txt') as json_file:
     data = json.load(json_file)
     for comment in data:
-        new_dict[comment['id']] = {
+        new_dict["data"].append({
             "body": comment['body'],
             "url": comment['url'].replace("commitcomment-", "r"),
             "interview": comment['path']
-        }
-        
+        })
+
 with open('processed_comments.txt', 'w') as outfile:
     json.dump(new_dict, outfile)
 
